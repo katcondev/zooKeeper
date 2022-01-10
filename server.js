a49d6fd54f2191b93707755f80b424d75c342729
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-01-09 22:22:39 -0800

    added default port

--- server.js
+++ server.js
@@ -95,6 +95,11 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html')); 
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
